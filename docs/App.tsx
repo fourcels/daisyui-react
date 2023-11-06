e85157d79223c64a8e6d509caff1fa2800e2c65f
Kiyan <fourcels@gmail.com>
2023-11-06 17:31:58 +0800

    highlight

--- docs/App.tsx
+++ docs/App.tsx
@@ -1,23 +1,11 @@
-import './App.css'
-import { Button } from '../components'
+// import './App.css'
+
+import Button from '../components/Button/Button.mdx'
 
 function App() {
 
   return (
-    <div className='flex flex-col items-center gap-4'>
-      <Button>Button</Button>
-      <div className='flex gap-2 flex-wrap'>
-        <Button>Button</Button>
-        <Button color='neutral'>neutral</Button>
-        <Button color='primary'>primary</Button>
-        <Button color='secondary'>secondary</Button>
-        <Button color='accent'>accent</Button>
-        <Button color='ghost'>ghost</Button>
-      </div>
-      <div className='flex gap-2 flex-wrap'>
-        <Button loading>Button</Button>
-      </div>
-    </div>
+    <Button />
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'node:path'
 import react from '@vitejs/plugin-react-swc'
+import mdx from '@mdx-js/rollup'
+import remarkToc from 'remark-toc'
+import rehypePrism from '@mapbox/rehype-prism'
+import rehypeSlug from 'rehype-slug'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +19,11 @@ export default defineConfig({
       external: ['react', 'react/jsx-runtime'],
     }
   },
-  plugins: [react()],
+  plugins: [
+    mdx({
+      remarkPlugins: [remarkToc],
+      rehypePlugins: [rehypeSlug],
+    }),
+    react(),
+  ],
 })
