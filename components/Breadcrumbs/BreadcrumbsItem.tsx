7ace5f9f52ef4f31641465f18ea0473bab8f3240
Kiyan <fourcels@gmail.com>
2024-02-26 11:23:30 +0800

    breadcumbs icon

--- components/Breadcrumbs/BreadcrumbsItem.tsx
+++ components/Breadcrumbs/BreadcrumbsItem.tsx
@@ -1,11 +1,29 @@
 import React, { forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-export type BreadcrumbsItemProps = React.LiHTMLAttributes<HTMLLIElement>;
+export type BreadcrumbsItemProps = React.LiHTMLAttributes<HTMLLIElement> & {
+  icon: React.ReactNode;
+};
 
 export const BreadcrumbsItem = forwardRef<HTMLLIElement, BreadcrumbsItemProps>(
-  (props, ref): JSX.Element => {
-    return <li {...props} ref={ref}></li>;
+  ({ icon, children, ...props }, ref): JSX.Element => {
+    const content = React.useMemo(() => {
+      if (!icon) {
+        return children;
+      }
+      return (
+        <div className="flex items-center gap-2">
+          {icon}
+          {children}
+        </div>
+      );
+    }, [children, icon]);
+
+    return (
+      <li {...props} ref={ref}>
+        {content}
+      </li>
+    );
   }
 );
 
