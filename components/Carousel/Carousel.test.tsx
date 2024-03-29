5a836f6f8d55259d4c5b582fc9df252becbc0614
Kiyan <fourcels@gmail.com>
2024-03-29 17:38:50 +0800

    carousel test

--- components/Carousel/Carousel.test.tsx
+++ components/Carousel/Carousel.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { createRef } from "react";
 import { Carousel } from "./Carousel";
 
@@ -13,5 +13,41 @@ describe("Carousel", () => {
     const ref = createRef<HTMLDivElement>();
     render(<Carousel ref={ref} />);
     expect(ref.current).toBeInTheDocument();
+  });
+  it("renders with a snap", () => {
+    const { getByTestId } = render(
+      <Carousel snap="start" data-testid="Carousel" />
+    );
+    expect(getByTestId("Carousel")).toHaveClass("carousel-start");
+  });
+  it("renders with vertical", () => {
+    const { getByTestId } = render(
+      <Carousel vertical data-testid="Carousel" />
+    );
+    expect(getByTestId("Carousel")).toHaveClass("carousel-vertical");
+  });
+  it("renders with vertical", () => {
+    const { getByTestId } = render(
+      <Carousel vertical data-testid="Carousel" />
+    );
+    expect(getByTestId("Carousel")).toHaveClass("carousel-vertical");
+  });
+  it("renders with width", () => {
+    const { getByTestId } = render(
+      <Carousel width="full">
+        <Carousel.Item data-testid="Carousel.Item"></Carousel.Item>
+      </Carousel>
+    );
+    expect(getByTestId("Carousel.Item")).toHaveClass("w-full");
+  });
+  it("renders with actions", () => {
+    const { container } = render(
+      <Carousel actions>
+        <Carousel.Item></Carousel.Item>
+        <Carousel.Item></Carousel.Item>
+      </Carousel>
+    );
+    expect(container.querySelector(".carousel-prev")).toBeInTheDocument();
+    expect(container.querySelector(".carousel-next")).toBeInTheDocument();
   });
 });
